e65f9519cc52267b847e6c4fac6cd863ece0fc52
Josh <joshvantsant@gmail.com>
2026-05-05 17:11:24 +0200

    Add photographerInfo object with details

---
+++ src/data/photographer.ts
@@ -0,0 +1,30 @@
+import type { PhotographerInfo } from '@/types';
+
+export const photographerInfo: PhotographerInfo = {
+  name: 'Josh van \'t Sant',
+  tagline: 'Design Engineer',
+  heroIntroduction: 'Passionate about designing soft bio-inspired robotics.',
+  biography: `Josh van \'t Sant is a problem solver with a varied experience in consulting and project management. Passionate about design, he has currently pursuing further research work in the field of soft robotics.',
+  awards: [
+    'International Photography Awards - Editorial Photographer of the Year 2023',
+    'PDN Photo Annual - Winner 2022',
+    'Hasselblad Masters Finalist 2021',
+    'Communication Arts Photography Annual 2020'
+  ],
+  experience: [
+    'TU Delft',
+    'ISISpace,
+    'Flexous'
+  ],
+  education: 'Master in Mechanical Engineering, TU Delft',
+  location: 'Delft, NL',
+  email: 'J.W.vantSant@tudelft.nl',
+  // phone: '+1 (555) 123-4567',
+  // availability: 'Currently booking for Q2 2025',
+  socialLinks: {
+    github: 'https://github.com/joshvantsant',
+    linkedin: 'https://linkedin.com/in/joshvantsant'
+  },
+  // Photo by Joshua Sun on Unsplash
+  // portraitImage: 'https://images.unsplash.com/photo-1585279968680-6606ee6ed134?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MDA2OTF8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjI3Njk1NjB8&ixlib=rb-4.1.0&q=80&w=1080'
+};
