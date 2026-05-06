5c980125257473d3cd7d9cbd7532c2e38d9cd886
Josh <joshvantsant@gmail.com>
2026-05-06 20:37:26 +0200

    Create Experience.tsx

---
+++ src/pages/Experience.tsx
@@ -0,0 +1,37 @@
+const experience = [
+  {
+    type: 'education',
+    title: 'MSc in Mechanical Engineering',
+    organisation: 'TU Delft',
+    period: '2022 – 2024',
+    description: 'Specialisation in medical devices and compliant mechanisms.',
+  },
+  {
+    type: 'work',
+    title: 'Research Assistant',
+    organisation: 'TU Delft',
+    period: 'Jul \'25 - current',
+    description: 'Research Assistant at the BioMorphic Intillence Lab developing soft robotic grippers',
+  },
+  {
+    type: 'work',
+    title: 'Space & Mechanical Design Engineer',
+    organisation: 'ISISpace',
+    period: 'Jul \'25 - Dec \'25',
+    description: 'Varied interdisciplinary engineering work on hardware and design of space equipment',
+  },
+  {
+    type: 'work',
+    title: 'Engineering Consultant|Project Manager',
+    organisation: 'Health New Zealand | Te Whatu Ora Te Matau a Māui',
+    period: 'Jul \'20 - Aug \'22',
+    description: 'Varied interdisciplinary engineering and project management work at the main hospital in Hawke\'s Bay',
+  },
+  {
+    type: 'work',
+    title: 'Mechanical Consultant Engineer',
+    organisation: 'Brevity',
+    period: 'Jan \'18 - Jan \'20',
+    description: 'Designed and simulated mechanical components for various companies',
+  }
+];
